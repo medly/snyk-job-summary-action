06f7bba498287c5196880fe71b94e590d115171c
cs250073 <christianaxel.schmidt@teradata.com>
2025-03-07 14:16:13 +0100

    feat: added container test and artifact name support

--- job_summary.py
+++ job_summary.py
@@ -23,6 +23,7 @@ severity_priority_list = ['critical', 'high', 'medium', 'low']
 
 SNYK_DEPENDENCIES_PATH = 'snyk_dependencies.json'
 SNYK_CODE_PATH = 'snyk_code.json'
+SNYK_CONTAINER_PATH = 'snyk_container.json'
 
 descriptions = {
         "code": {
@@ -38,6 +39,10 @@ descriptions = {
         "licenses":{
             "resources": [("Licenses","https://docs.snyk.io/products/snyk-open-source/licenses")],
             "summary": "Every time you push your code, your repositories are scanned not only for vulnerabilities but also for license compliance. This includes all of your direct and indirect dependencies. Snyk scans your manifest files, and then checks for license issues against Snyk’s known licenses."
+        },
+        "container":{
+            "resources": [("Snyk container","https://docs.snyk.io/scan-with-snyk/snyk-container")],
+            "summary": "Every time you push your container image, Snyk Container provides tools and integrations to quickly find and fix vulnerabilities. This allows you to create images that have security built-in from the start."
         }
     }
 
@@ -114,6 +119,31 @@ def get_code_vulnerability_count(snyk_code_data):
 
     return synk_code_count
 
+def get_container_vulnerability_count(snyk_container_data):
+    """
+    Takes the container file created by snyk and count container vulnerabilities
+
+    Args:
+        SNYK_CONTAINER_PATH (string): path of snyk code file which is created using `snyk container test --json-file-output=snyk_container.json`
+
+    Returns:
+        count of container vulnerability with respect to severity
+    """
+    if not isinstance(snyk_container_data, list):
+        snyk_container_data = [snyk_container_data]
+
+    synk_container_count = {}
+
+    for data in snyk_container_data:
+        synk_container_results = data["runs"][0]["results"]
+
+        # count the different level of vulnerability
+        for container_vulnerability in synk_container_results:
+            vulnerability_level = code_severity_map[container_vulnerability["level"].lower()]
+            synk_container_count[vulnerability_level] = synk_container_count.get(
+                vulnerability_level, 0) + 1
+
+    return synk_container_count
 
 def dump_vulnerabilities(vulnerabilities_count, type, output, resources, summary):
     """
@@ -121,8 +151,11 @@ def dump_vulnerabilities(vulnerabilities_count, type, output, resources, summary
     Args:
         vulnerabilities_count (dict): store the count with respect to severity eg. {'high' : 10, 'low' : 16}
         type (string): type of vulnerability
-        output (MdUtils): output .md file  
+        output (MdUtils): output .md file
+        resources (list): list of resources
+        summary (string): summary description
     """
+    json_output = {"list": []}
     output.new_header(level=1, title=f"{type.capitalize()} Scanner Result Summary")
     output.new_line(summary, bold_italics_code='')
     total = 0
@@ -143,12 +176,17 @@ def dump_vulnerabilities(vulnerabilities_count, type, output, resources, summary
         symbol = severity_symbol_map.get(vulnerability_type, "")
         display_list.extend(
             [symbol + vulnerability_type.capitalize(), str(count)])
+        json_output["list"].append({"type": symbol + vulnerability_type.capitalize(), "count": int(count)})
 
     display_list.extend(["Total", total])
+    json_output["total"] = total
 
     output.new_line("Finding Summary", bold_italics_code='cib')
-    
+
     output.new_table(columns=2, rows=len(display_list)//2, text=display_list)
+
+    with open(f'{type}_summary.json', 'w', encoding='utf-8') as f:
+        json.dump(json_output, f, ensure_ascii=False, indent=4)
 
 
 def display_count(vulnerability_data, output, type_of_vulnerability):
@@ -157,18 +195,24 @@ def display_count(vulnerability_data, output, type_of_vulnerability):
     Args:
         vulnerability_data (json)
         output (MdUtils): output file ('vulnerabilities.md')
-        type_of_vulnerability (string): threee types -> ['code', 'dependencies', 'licenses']
+        type_of_vulnerability (string): three types -> ['code', 'dependencies', 'container']
     """    
     if vulnerability_data:
         if type_of_vulnerability == 'code':
             snyk_code_count = get_code_vulnerability_count(vulnerability_data)
             display_vulnerabilities(output, snyk_code_count, type_of_vulnerability)
+        if type_of_vulnerability == 'container':
+            snyk_container_count = get_container_vulnerability_count(vulnerability_data)
+            display_vulnerabilities(output, snyk_container_count, type_of_vulnerability)
         elif type_of_vulnerability == 'dependencies':
             snyk_dependencies_count, synk_licenses_count = get_dependencies_and_licenses_vulnerability_count(vulnerability_data)
             display_vulnerabilities(output, snyk_dependencies_count, type_of_vulnerability)
             display_vulnerabilities(output, synk_licenses_count, 'licenses')
     else:
         if type_of_vulnerability == "code":
+            output.new_header(level=1, title=f"{type_of_vulnerability.capitalize()} Scanner Result Summary")
+            output.new_paragraph("No Vulnerabilities Found")
+        if type_of_vulnerability == "container":
             output.new_header(level=1, title=f"{type_of_vulnerability.capitalize()} Scanner Result Summary")
             output.new_paragraph("No Vulnerabilities Found")
         elif type_of_vulnerability == "dependencies":
@@ -194,13 +238,18 @@ try:
     
     if len(command_line_args) > 2 and command_line_args[2].strip() != "":
         SNYK_CODE_PATH = command_line_args[2]
-    
+
+    if len(command_line_args) > 3 and command_line_args[3].strip() != "":
+        SNYK_CONTAINER_PATH = command_line_args[3]
+
     output = MdUtils(file_name="vulnerabilities")
     snyk_dependency_data = get_json_object(SNYK_DEPENDENCIES_PATH)
     snyk_code_data = get_json_object(SNYK_CODE_PATH)
-    
+    snyk_container_data = get_json_object(SNYK_CONTAINER_PATH)
+
     display_count(snyk_code_data, output, "code")
     display_count(snyk_dependency_data, output, "dependencies")
+    display_count(snyk_container_data, output, "container")
     output.create_md_file()
 
 except Exception as err:
